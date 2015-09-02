8db903568b47897e621f129e8bc298c2b29c1a22
Steyn Huizinga <gitlab@steynhuizinga.nl>
2015-09-03 00:08:54 +0200

    Renamed tags for dockerhub

--- contrib/run.sh
+++ contrib/run.sh
@@ -3,6 +3,6 @@
 echo "Starting MariaDB ..."
 docker run --name spotwebdb -e MYSQL_ROOT_PASSWORD=rootpw -e MYSQL_USER=spotweb -e MYSQL_PASSWORD=spotweb -e MYSQL_DATABASE=spotweb -d mariadb
 echo "Starting PHP-FPM ..."
-docker run --name spotwebphp --link spotwebdb:mysql -d spotwebphp
+docker run --name spotwebphp --link spotwebdb:mysql -d steynovich/spotweb-php
 echo "Starting nginx ..."
-docker run --name spotwebnginx --link spotwebphp:fpm -p 80:80 -d spotwebnginx
+docker run --name spotwebnginx --link spotwebphp:fpm -p 80:80 -d steynovich/spotweb-nginx
